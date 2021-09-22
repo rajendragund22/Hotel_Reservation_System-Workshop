c36663e7d938d488218774a7624fb8d65ce21ea2
RajendraGund <rajendragund2015@gmail.com>
2021-09-22 14:20:30 +0530

    Added : Welcome message for the User

---
+++ src/main/java/com/bridgelabz/workshop/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.workshop;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Reservation System Program!!!");
+    }
+}
