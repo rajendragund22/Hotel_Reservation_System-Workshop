b3fd31e86d068f1e9bb55c43ee699a347d04b112
RajendraGund <rajendragund2015@gmail.com>
2021-09-22 14:37:18 +0530

    Added : Program for the Find Cheapest Hotel for given time range

--- src/test/java/com/bridgelabz/workshop/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/workshop/HotelReservationTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class HotelReservationTest {
@@ -24,5 +26,19 @@ public class HotelReservationTest {
         hotelReservation.add(hotel3);
         List<Hotel> result = hotelReservation.getHotelList();
         assertEquals(hotels, result);
+    }
+    @Test
+    public void whenGivenDateRangeShouldReturnCheapestHotel()
+    {
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add(hotel1);
+        hotelReservation.add(hotel2);
+        hotelReservation.add(hotel3);
+        Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+        result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+        assertNotNull(result);
     }
 }
